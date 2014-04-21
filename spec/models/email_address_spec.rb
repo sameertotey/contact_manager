535ad60104cf16d020949fe295ea7d848aea5531
Sameer Totey <sameertotey@hotmail.com>
2014-04-21 17:15:12 -0600

    added some more email specs

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  let(:email_address){EmailAddress.new(address: "someone@example.com")}
+  let(:email_address){EmailAddress.new(address: "someone@example.com", person_id: 1)}
 
   it 'is valid' do
     expect(email_address).to be_valid
@@ -10,5 +10,11 @@ describe EmailAddress do
     email_address.address = nil
     expect(email_address).to_not be_valid
   end
-
+   it 'must have a reference to a person' do
+    email_address.person_id = nil
+    expect(email_address).to_not be_valid
+  end
+  it 'is associated with a person' do
+    expect(email_address).to respond_to(:person)
+  end
 end
